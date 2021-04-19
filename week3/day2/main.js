0fbea2be81311f28a3a0305412917085a5ba815a
dan-emmanuel <d.e.azoulay@gmail.com>
2021-04-20 00:46:16 +0300

    ex day2

---
+++ week3/day2/main.js
@@ -0,0 +1,39 @@
+5 + "34" 
+// output 534
+console.log(5 + "34")
+5 - "4"
+// output 1
+console.log(5 - "4")
+10 % 5
+// output 0
+console.log(10 % 5)
+5 % 10
+// output 5
+console.log(5 % 10)
+"Java" + "Script"
+// output "JavaScript"
+console.log("Java" + "Script")
+" " + " "
+// output "  "
+console.log(" " + " ")
+" " + 0
+// output " 0"
+console.log(" " + 0)
+true + true
+// output 2
+console.log(true + true)
+true + false
+// output 1
+console.log(true + false)
+false + true
+// output 1
+console.log(false + true)
+false - true
+// output -1
+console.log(false - true)
+3 - 4
+// output 1
+console.log(3 - 4)
+"Bob" - "bill"
+// output NaN
+console.log("Bob" - "bill")
