60991379e3754b9bc9ba95a5d6946244e12f4e84
dan-emmanuel <d.e.azoulay@gmail.com>
2021-04-25 18:22:32 +0300

    update on portFolio

--- week3/day5/dailychallenge1/main.js
+++ week3/day5/dailychallenge1/main.js
@@ -6,10 +6,13 @@ for (let index = 0; index < 6; index++) {
     console.log(text)
 }
 // then by using two nested for loops (Nested means one inside the other - check out “nested for loops” on Google).
-for (let i = 0; i < 6; i++) {
+   
+for (let i = 1; i <=6 ; i++) {
     let text =""
-    for (let j = 0; j <=i; j++) {
+    for (let j = 1; j <=6; j++) {
         text+="*"
     };
     console.log(text)
+    console.log("")
+
 }
