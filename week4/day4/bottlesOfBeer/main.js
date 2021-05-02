a6139eda900cbb3e1d7d5e0c45d6aa31a908835b
dan-emmanuel <d.e.azoulay@gmail.com>
2021-05-02 15:30:31 +0300

    bottle of beer on the wall

--- week4/day4/bottlesOfBeer/main.js
+++ week4/day4/bottlesOfBeer/main.js
@@ -5,13 +5,10 @@ let toRemove = 1
 while (numberOfBottles>0) {
 	console.log(`${numberOfBottles} bottles of beer on the wall \n
 ${numberOfBottles} bottles of beer \n`)
-
 	toRemove = (numberOfBottles-toRemove)>0?toRemove:numberOfBottles
-
 	console.log(`Take ${toRemove} down, pass ${toRemove==1?`it`:`them`} around`)
 	numberOfBottles -=toRemove
 	console.log(numberOfBottles>0?`${numberOfBottles} bottles of beer on the wall`:`no more bottles of beer on the wall`)
 	toRemove+=1
-	
 }
 
