6ff6466797f9c83680c8cdccc56fb9ceda8c4ca9
dan-emmanuel <d.e.azoulay@gmail.com>
2021-05-02 19:32:04 +0300

    planetsdailyC

---
+++ week4/day5/planets/script.js
@@ -0,0 +1,23 @@
+let planets = [{name:"Mercure",moons:[]},{name:"Venus",moons:[]},{name:"Earth",moons:["moon"]},{name:'Mars',moons:["Deimos","Phobos"]},{name:'Jupiter',moons:["IO","Europa","Ganymede"]},{name:'Sarturna',moons:["Ymir","Paaliaq","Siarnaq","Tarvos","Kiviuq","Ijiraq","Thrymr"]}, {name:'Uranus',moons:["Juliette", "Portia", "Cressida", "Desdémone", "Rosalinde", "Belinda", "Cordélia"]},{name:'Neptune',moons:["Despina","Galatea","HippoCamp"]}];
+
+planets.forEach(planet => {
+    let divPlanet = document.createElement("div")
+    divPlanet.classList.add("planet")
+    divPlanet.classList.add(planet.name)
+
+    let planetTextNode = document.createTextNode(planet.name)
+    divPlanet.appendChild(planetTextNode)
+    document.querySelector(`.listPlanets`).append(divPlanet)
+    planet.moons.forEach(moon => {
+        let divMoon = document.createElement("div")
+        divMoon.classList.add("moon")
+        let moonTextNode = document.createTextNode(moon)
+        divMoon.appendChild(moonTextNode)
+        divPlanet.append(divMoon)
+    });
+    
+});
+
+
+
+
