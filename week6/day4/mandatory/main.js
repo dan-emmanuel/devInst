7ac78d0f547d948758b6b03f9ddd56205c9a9990
dan-emmanuel <d.e.azoulay@gmail.com>
2021-05-18 00:17:46 +0300

    update

---
+++ week6/day4/mandatory/main.js
@@ -0,0 +1,36 @@
+// ex1
+let sum = (x,y)=>x+y 
+
+
+
+// ex2
+// const addTo = x => y => x + y;
+// var addToTen = addTo(10);
+// addToTen(3);
+
+// 13
+
+
+
+// ex3  
+// const sum = (a, b) => a + b
+// const curriedSum = (a) => (b) => a + b
+// curriedSum(30)(1)
+// 31
+
+
+
+
+// ex4
+// const sum = (a, b) => a + b
+// const curriedSum = (a) => (b) => a + b
+// const add5 = curriedSum(5)
+// add5(12)
+// 17
+
+// ex5
+//const compose = (f, g) => (a) => f(g(a));
+// const add1 = (num) => num + 1;
+// const add5 = (num) => num + 5;
+// compose(add1, add5)(10)
+// 16
