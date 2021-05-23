5492e9046ec5632994efaf949981d2bcd23a4046
dan-emmanuel <d.e.azoulay@gmail.com>
2021-05-23 15:56:54 +0300

    update

--- week6/day4/ninja/main.js
+++ week6/day4/ninja/main.js
@@ -7,12 +7,17 @@
 // should return 'There is no spoon.'
 // Below is a verbose JavaScript solution, turn this into a currying function.
 
-// function mergeWords(string) {
-//  return function(nextString) {
-//    if (nextString === undefined) {
-//      return string;
-//    } else {
-//      return mergeWords(string + ' ' + nextString);
-//    }
-//  }
-// }
+function mergeWords(string) {
+    return function(nextString) {
+      if (nextString === undefined) {
+        return string;
+      } else {
+        return mergeWords(string + ' ' + nextString);
+      }
+    }
+}
+   
+
+
+let  mergeWordsNway = string => string2=> string2==undefined?string:mergeWords(`${string} ${string2}`)
+console.log(mergeWords('There')('is')('no')('spoon.')())
