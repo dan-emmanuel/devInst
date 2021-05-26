5b7172bf5485401adb5339d1ea1a63b5ac2c7882
dan-emmanuel <d.e.azoulay@gmail.com>
2021-05-26 21:39:06 +0300

    update

--- week7/day4/gold/main.js
+++ week7/day4/gold/main.js
@@ -1,61 +1,47 @@
+let printFullName =({firs,last}) =>`Your full name is ${firs} ${last}`
 
-// ex1
-[1, 2, 3].map(num => {
-    if (typeof num === 'number') return num * 2;
-    return ;
-  });
-
-//   [2,4,6]
+console.log(printFullName({first: 'Elie', last:'Schoppik'})) 
 
 
 
-
-// ex2
-[[0, 1], [2, 3]].reduce(
-    (acc, cur) => {
-      return acc.concat(cur);
-    },
-    [1, 2],
+let keysAndValues = (object)=>{
+  let ordered = Object.keys(object).sort().reduce(
+    (obj, key) => { 
+      obj[key] = object[key]; 
+      return obj;
+    }, 
+    {}
   );
+  return [Object.keys(ordered),Object.values(ordered)]
 
-//   [1,2,0,1,2,3]
+}
+console.log(keysAndValues({ a: 1, b: 2, c: 3 }))
+// [["a", "b", "c"], [1, 2, 3]]
 
-// ex3
+console.log(keysAndValues({  b: "Microsoft", c: "Google",a: "Apple" }))
+// [["a", "b", "c"], ["Apple", "Microsoft", "Google"]]
 
-const arrayNum = [1, 2, 4, 5, 8, 9];
-const newArray = arrayNum.map((num, i) => {
-    console.log(num, i);
-// i = 0
-// i = 1
-// i = 2
-// i = 3
-// i = 4
-// i = 5
-
-    // alert(num);
-    return num * 2;
-})
-
-// i = undefined
-
-// ex4
-const array = [[1],[2],[3],[[[4]]],[[[5]]]]
-
-let array2 = array.map(e=>e[0])
-console.log(array2)
+console.log(keysAndValues({ key1: true, key2: false, key3: undefined }))
+//  [["key1", "key2", "key3"], [true, false, undefined]]
 
 
 
+class Counter {
+  constructor() {
+    this.count = 0;
+  }
 
-const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
+  increment() {
+    this.count++;
+  }
+}
 
-let greeting2 = greeting.map(e=>e.join(""))
+const counterOne = new Counter();
+counterOne.increment();
+counterOne.increment();
 
-let greetingstring  = greeting2.join(" ")
+const counterTwo = counterOne;
+counterTwo.increment();
 
-
-const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]]
-let flatDeep =(array)=>array.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val) : val), [])
-let trapped2 = flatDeep(trapped)
-
-console.log(trapped2)
+console.log(counterOne.count);
+// 3
