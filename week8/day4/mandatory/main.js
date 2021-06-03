11fbbe142509efd3ba2d783a8afa8b18a79c074a
dan-emmanuel <d.e.azoulay@gmail.com>
2021-06-03 20:32:19 +0300

    update

---
+++ week8/day4/mandatory/main.js
@@ -0,0 +1,16 @@
+// ex1
+
+let getdata = (params)=>{
+  var url = new URL("https://api.giphy.com/v1/gifs/search")
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+    return fetch(url).then(e=>e.json())
+}
+let logJson = (json)=>{console.log(json)}
+
+getdata(  params = {q:"hilarious",rating:"g",api_key:"hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"}).then(logJson)
+// ex2
+getdata(  params = {q:"hilarious",rating:"g",api_key:"hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My",limit:10}).then(logJson)
+
+
+
+
