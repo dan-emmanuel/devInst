72a19042b81a4df792aadc1d64601aaff25c98a5
dan-emmanuel <d.e.azoulay@gmail.com>
2021-06-03 21:25:59 +0300

    update

--- week8/day4/gold/main.js
+++ week8/day4/gold/main.js
@@ -5,15 +5,17 @@ let getdata = (params)=>{
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
     return fetch(url).then(e=>e.json()).then(result=>result.data)
 }
+
 let random = (arr)=>arr[Math.floor(Math.random() * arr.length)];
 let createagif = ({images:{original},title})=>{return Object.assign(document.createElement("img"),{alt:title,src:original.url,width:"250"} )}
 let executeAfter = e=>{ 
   let container =   document.querySelector(`#gifToShow`)
   container.innerHTML=""
-  container.append( createagif(random(e)))}
+  container.append( createagif(random(e)))
+}
 
 
- document.querySelector("#gifGen").onmousedown  =(e)=> getdata(  params = {q:"hilarious",rating:"g",api_key:"hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"}).then(executeAfter)
+ document.querySelector("#gifGen").onmousedown  =(e)=> getdata(params = {q:"hilarious",rating:"g",api_key:"hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"}).then(executeAfter)
 
 
 
