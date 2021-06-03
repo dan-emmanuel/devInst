72f57b443fdb45fc5d0afdfccfa255f737638e23
dan-emmanuel <d.e.azoulay@gmail.com>
2021-06-04 00:51:19 +0300

    update

--- week8/day4/gold/main.js
+++ week8/day4/gold/main.js
@@ -1,17 +1,15 @@
-// ex1
-
 let getdata = (params)=>{
   var url = new URL("https://api.giphy.com/v1/gifs/search")
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-    return fetch(url).then(e=>e.json())
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+  return fetch(url).then(e=>e.json()).then(result=>result.data)
 }
-let logJson = (json)=>{console.log(json)}
 
-getdata(  params = {q:"hilarious",rating:"g",api_key:"hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"}).then(logJson)
-// ex2
-getdata(  params = {q:"hilarious",rating:"g",api_key:"hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My",limit:10}).then(logJson)
+let random = (arr)=>arr[Math.floor(Math.random() * arr.length)];
+let createagif = ({images:{original},title})=>{return Object.assign(document.createElement("img"),{alt:title,src:original.url,width:"250"} )}
+let executeAfter = e=>{
+  let container =   document.querySelector(`#gifToShow`)
+  container.innerHTML=""
+  container.append( createagif(random(e)))
+}
 
-
-
-
-
+document.querySelector("#gifGen").onmousedown  =(e)=> getdata(params = {q:"hilarious",rating:"g",api_key:"hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"}).then(executeAfter)
