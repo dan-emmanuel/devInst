2b3efe4b85dceb920f3623b2c764e71190990904
dan-emmanuel <d.e.azoulay@gmail.com>
2021-07-06 18:20:39 +0300

    update

---
+++ week14/day2/ex2/src/H1.js
@@ -0,0 +1,7 @@
+function H1({text}) {
+    return (
+        <h1>{text}</h1>
+    );
+  }
+  
+  export default H1;
---
+++ week14/day2/ex2/src/TenTimes.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+function TenTimes() {
+  let sum = 5+5
+  return (
+    <div >
+      react is {sum} times better than jsx
+    </div>
+  );
+}
+
+export default TenTimes;
