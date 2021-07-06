b6d1a484476f2afc122d9b2d0cae202c2675c58f
dan-emmanuel <d.e.azoulay@gmail.com>
2021-07-06 19:11:51 +0300

    update

--- week14/day2/ex2/src/H1.js
+++
@@ -1,7 +0,0 @@
-function H1({text}) {
-    return (
-        <h1>{text}</h1>
-    );
-  }
-  
-  export default H1;
--- week14/day2/ex2/src/User.js
+++
@@ -1,9 +0,0 @@
-const User = ({name,age})=>{
-    return (
-        <div>
-            I'm {name} and I'm {age} years old
-        </div>
-    )
-}
-
-export default User
